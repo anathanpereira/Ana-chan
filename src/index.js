0b82d65c27a864b41d44e75a02995a78d90c21bc
Anathan <anathan.t.pereira@gmail.com>
2021-10-21 15:35:45 -0300

    Musica ta tocano BOA

--- src/index.js
+++ src/index.js
@@ -4,11 +4,31 @@
 
 require("dotenv").config();
 
-const { Client, Intents, Message } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_TYPING, Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
+const ytdl = require('ytdl-core');
+const ytSearch = require('yt-search');
+const { joinVoiceChannel, getVoiceConnection, createAudioPlayer, AudioResource, createAudioResource, StreamType } = require('@discordjs/voice');
+const { Client, Intents, Message, VoiceChannel } = require('discord.js');
+const client = new Client({
+  intents: [
+    Intents.FLAGS.DIRECT_MESSAGES,
+    Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+    Intents.FLAGS.DIRECT_MESSAGE_TYPING,
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_BANS,
+    Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+    Intents.FLAGS.GUILD_INTEGRATIONS,
+    Intents.FLAGS.GUILD_INVITES,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Intents.FLAGS.GUILD_MESSAGE_TYPING,
+    Intents.FLAGS.GUILD_PRESENCES,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+    Intents.FLAGS.GUILD_WEBHOOKS,
+  ],
+});
 
 const PREFIX ='a!'
-
 
 var servers = {};
 
@@ -16,12 +36,21 @@ const generateRandomNumber = (min, max) =>  {
     return Math.floor(Math.random() * (max - min) + min);
 };
 
-client.on('ready', ()=>{
+client.on('ready', () => {
     console.log(`${client.user.tag} has logged in`);
+    client.channels.fetch('703677055077843036').then(channel => {
+        channel.send("Patroa chegou 💁‍♀️")
+    });
 });
 
+client.on('invalidated', () => {
+    client.channels.fetch('703677055077843036').then(channel => {
+        channel.send("Patroa vazou xx");
+    });
+});
 
-client.on('message', (message) => {
+client.on('message', async (message) => {
+    console.log(message.members);
     console.log('['+ message.author.tag+'] '+message.content);
 
     if(message.content === 'oi ana-chan'){
@@ -49,28 +78,57 @@ client.on('message', (message) => {
         
     }
 
-
     let args = message.content.substring(PREFIX.length).split(" ");
 
     switch(args[0]){
         case 'p':
+            //validações
             if(!args[1]){
                 message.channel.send("Falto o link bobinho hihi");
                 return;
             }
-
-            if(!message.member.voiceChannel){
+            if(!message.member.voice.channel){
                 message.channel.send("Flor, vc tem q ta numa sala pra tocar musica");
                 return;
             }
 
-            if(!servers[message.guild.id]) servers[message.guild.id] = {
-                queue: []
+            //entra na sala
+            let voiceChannel = message.member.voice.channel;
+            joinVoiceChannel({
+                channelId: message.member.voice.channel.id,
+                guildId: message.guild.id,
+                adapterCreator: message.guild.voiceAdapterCreator
+            });
+
+
+            const connection = getVoiceConnection(message.guild.id);
+
+            //busca no yt
+            const videoFinder = async (query) => {
+                const videoResult = await ytSearch(query);
+
+                return (videoResult.videos.length > 1) ? videoResult.videos[0] : null;
+            }
+
+            args.shift()
+            const video = await videoFinder(args.join(' '));
+
+            if (video) {
+                const stream = await ytdl(video.url, {filter: 'audioonly'});
+               
+                const resource = createAudioResource(stream, {
+                    inputType: StreamType.Arbitrary,
+                });
+
+                const player = createAudioPlayer();
+                player.play(resource);
+                connection.subscribe(player);
             }
             
-            var server = servers[message.guild.id];
+            break;
 
-
+        case 'd':
+            connection.destroy();
     }
 
     
